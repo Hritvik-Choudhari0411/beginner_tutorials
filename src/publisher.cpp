c16b55b46a45cb152ee3ebab783b8b7f0dd560a0
Hritvik Choudhari <hritvikchoudhari@gmail.com>
2023-11-13 21:33:47 -0500

    Modefied publisher and subscriber code to inculcate services

---
+++ src/publisher.cpp
@@ -0,0 +1,175 @@
+/******************************************************************************
+  *MIT License
+
+  *Copyright (c) 2022
+
+  *Permission is hereby granted, free of charge, to any person obtaining a copy
+  *of this software and associated documentation files (the "Software"), to deal
+  *in the Software without restriction, including without limitation the rights
+  *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  *copies of the Software, and to permit persons to whom the Software is
+  *furnished to do so, subject to the following conditions:
+
+  *The above copyright notice and this permission notice shall be included in all
+  *copies or substantial portions of the Software.
+
+  *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  *SOFTWARE.
+  ******************************************************************************/
+/**
+ * @file publisher.cpp
+ * @author Hritvik Choudhari (hac@umd.edu)
+ * @brief ROS services - Publisher node
+ * @version 0.2
+ * @date 2023-11-13
+ *
+ * @copyright MIT Copyright (c) 2022
+ *
+ */
+
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "beginner_tutorials/srv/string_mod.hpp"
+
+using namespace std::chrono_literals;
+/**
+ * @brief The Class definition for the node minimal_publisher
+ *
+ */
+class MinimalPublisher : public rclcpp::Node {
+ public:
+  /**
+   * @brief Construct a new Minimal Publisher object
+   *
+   */
+  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+    this->declare_parameter("Publisher_Frequency",
+                            1.0);  // Declare the parameter Publisher_Frequency
+    publisher_ = this->create_publisher<std_msgs::msg::String>(
+        "topic", 10);  // 10 is the queue size
+
+    double freq = 1.0;  // Default frequency
+
+    if (this->get_parameter("Publisher_Frequency")
+            .get_parameter_value()
+            .get<double>() > 0.0) {  // Check if the frequency is greater
+                                    // than 0, set the frequency to
+                                   // the parameter value
+      freq = this->get_parameter("Publisher_Frequency")
+                        .get_parameter_value()
+                        .get<double>();
+      RCLCPP_DEBUG_STREAM(this->get_logger(), "Talker publishing at "
+                                                  << freq << " Hz"
+                                                  << std::endl);
+    } else if (this->get_parameter("Publisher_Frequency")
+                   .get_parameter_value()
+                   .get<double>() < 0.0) {  // If the frequency is negative, set
+                                           // it to the default value
+      RCLCPP_FATAL_STREAM(this->get_logger(),
+                          "Positive value of frequency expected" << std::endl);
+      RCLCPP_WARN_STREAM(this->get_logger(),
+                         "Talker frequency set to default value of 1.0Hz"
+                             << std::endl);
+      freq = 1.0;
+    } else if (this->get_parameter("Publisher_Frequency")
+                   .get_parameter_value()
+                   .get<double>() ==
+               0.0) {  // If the frequency is zero, set it to the default value
+      RCLCPP_FATAL_STREAM(this->get_logger(),
+                          "Frequency cant be zero" << std::endl);
+      RCLCPP_WARN_STREAM(this->get_logger(),
+                         "Talker frequency set to default value of 1.0Hz"
+                             << std::endl);
+      freq = 1.0;
+    }
+
+    timer_ = this->create_wall_timer(
+        std::chrono::duration<double>(freq),
+        std::bind(&MinimalPublisher::timer_cb, this));
+    server_ = this->create_service<beginner_tutorials::srv::StringMod>(
+        "string_change",
+        std::bind(&MinimalPublisher::string_mod_cb, this,
+                  std::placeholders::_1, std::placeholders::_2));
+  }
+
+ private:
+  /**
+   * @brief Callback function for the timer which will loop the publisher and
+   * publish the message
+   *
+   */
+  void timer_cb() {
+    auto message = std_msgs::msg::String();
+    message.data = msg + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Service<beginner_tutorials::srv::StringMod>::SharedPtr
+      server_;
+  size_t count_;
+  std::string msg = "Message published : ";  // Default message
+  /**
+   * @brief service callback function to change the message being published
+   *
+   * @param request  The service request
+   * @param response  The service response
+   */
+  void string_mod_cb(
+      const beginner_tutorials::srv::StringMod::Request::SharedPtr request,
+      const beginner_tutorials::srv::StringMod::Response::SharedPtr
+          response) {
+    if (request->input == "reset") {  // If the input is reset, set the message
+                                     // to the default message
+      RCLCPP_WARN_STREAM(this->get_logger(),
+                         "Resetting the string" << std::endl);
+      RCLCPP_INFO_STREAM(this->get_logger(),
+                         "New string is:" << msg << std::endl);
+      msg = "Message published : ";
+      response->output = msg;
+    } else if (request->input.empty()) {  // If the input is empty, set the
+                                         // message to the default message
+      RCLCPP_ERROR_STREAM(this->get_logger(),
+                          "Empty string received" << std::endl);
+      RCLCPP_WARN_STREAM(this->get_logger(),
+                         "No change will be made to the original string"
+                             << std::endl);
+    } else {  // If the input is not empty or reset,
+             // set the message to the input
+      msg = request->input;
+      RCLCPP_INFO_STREAM(this->get_logger(), "New string received");
+      response->output = msg;
+      RCLCPP_INFO_STREAM(this->get_logger(),
+                         "Incoming request\nThe New String is: "
+                             << msg.c_str() << std::endl);
+      RCLCPP_INFO_STREAM(this->get_logger(), "Sending response to talker: "
+                                                 << msg.c_str() << std::endl);
+    }
+  }
+};
+/**
+ * @brief Main function for the node which initializes the node and spins it
+ *
+ * @param argc  The number of arguments
+ * @param argv  The arguments
+ * @return int  The return value
+ */
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/subscriber.cpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+  *MIT License
+
+  *Copyright (c) 2022
+
+  *Permission is hereby granted, free of charge, to any person obtaining a copy
+  *of this software and associated documentation files (the "Software"), to deal
+  *in the Software without restriction, including without limitation the rights
+  *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  *copies of the Software, and to permit persons to whom the Software is
+  *furnished to do so, subject to the following conditions:
+
+  *The above copyright notice and this permission notice shall be included in all
+  *copies or substantial portions of the Software.
+
+  *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  *IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  *FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  *AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  *SOFTWARE.
+  ******************************************************************************/
+/**
+ * @file subscriber.cpp
+ * @author Hritvik Choudhari (hac@umd.edu)
+ * @brief ROS service - Suscriber node
+ * @version 0.12
+ * @date 2022-11-13
+ *
+ * @copyright MIT Copyright (c) 2022
+ *
+ */
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+/**
+ * @brief The Class definition for the node minimal_subscriber
+ *
+ */
+class MinimalSubscriber : public rclcpp::Node {
+ public:
+  /**
+   * @brief Construct a new Minimal Subscriber object
+   *
+   */
+  MinimalSubscriber() : Node("minimal_subscriber") {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+        "topic", 10, std::bind(&MinimalSubscriber::topic_cb, this, _1));
+  }
+
+ private:
+  /**
+   * @brief Callback funtion to print what the listener heard from the publisher
+   *
+   * @param msg  The message that the listener heard from the publisher
+   */
+  void topic_cb(const std_msgs::msg::String &msg) const {
+    RCLCPP_INFO(this->get_logger(), "Message received: %s", msg.data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+/**
+ * @brief Main function for the node minimal_subscriber that initializes the
+ * node and spins it
+ *
+ * @param argc  Number of arguments  (Argument Count)
+ * @param argv  Arguments (Argument vector)
+ * @return int  Returns 0 on successful execution
+ */
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
